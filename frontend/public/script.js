4171d02c0b0cb4353e7952015462dd21fcaebd70
Bundicharles <bundimunenecharles6206@gmail.com>
2025-05-16 12:32:07 +0300

    all

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -417,16 +417,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then((reg) => {
-          console.log("Service Worker registered.", reg);
-        })
-        .catch((err) => {
-          console.error("Service Worker registration failed:", err);
-        });
-    });
-  }
-  
